543e5150f7e54f4dbb443779f7e8161a65f7662b
dimashmuel <dimashmuel12@gmail.com>
2026-05-31 17:42:46 +0300

    Implement fallback 404 route with custom NotFound component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/home';
 import AboutPage from './pages/about';
 import { Routes, Route } from 'react-router';
 import Header from './components/Header';
+import NotFoundPage from './pages/not-found';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -51,6 +52,8 @@ const App = () => {
           />
         } />
         <Route path='/about' element={<AboutPage />} />
+        
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </>
   );
