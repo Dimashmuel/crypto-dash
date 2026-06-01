2d5e659c21ffb07e963ad02b26ca5bf430ee8d04
dimashmuel <dimashmuel12@gmail.com>
2026-06-01 10:13:22 +0300

    Add Spinner component to improve UX during API calls

--- src/pages/coin-details.jsx
+++ src/pages/coin-details.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router';
+import Spinner from '../components/Spinner';
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
@@ -37,7 +38,7 @@ const CoinDetailsPage = () => {
                 {coin ? `${coin.name} (${coin.symbol.toUpperCase()})` : 'Coin Details'}
             </h1>
 
-            {loading && <p>Loading...</p>}
+            {loading && <Spinner />}
             {error && <p className='error'>❌ {error}</p>}
 
             {!loading && !error && coin && (
